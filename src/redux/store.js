66a68e64eed482a0a8e27bf9b3db433f7b9c994e
tranvnb <brianvo@BrianHome.localdomain>
2021-04-26 16:43:42 -0700

    add thunk and initial state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import counterReducer from './counterRedux';
 
 
@@ -6,7 +7,24 @@ const rootReducers = combineReducers({
     counterReducer: counterReducer
 })
 
+// add add dev tool to composer
+const devtoolCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// apply middleware as an enhancer
+const middlewareEnhancer = applyMiddleware(thunkMiddleware);
+
+// compose all enhancers
+const composeEnhancers = devtoolCompose(middlewareEnhancer);
+
+// initialize state
+const initialState = {
+    counterReducer: {
+        number: -1
+    }
+}
+
 export default createStore(
     rootReducers, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    initialState,
+    composeEnhancers
 );
