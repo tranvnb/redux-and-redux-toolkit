3825534a2cae787e9f30ea0ef6b9c1fcb5d77425
tranvnb <brianvo@BrianHome.localdomain>
2021-04-26 17:44:18 -0700

    add thunk and related actions (start, loading, succeed, failed)

--- src/components/CounterRedux/counterRedux.js
+++ src/components/CounterRedux/counterRedux.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { actionIncrease, actionDecrease } from '../../redux/counterRedux'
+import { actionIncrease, actionDecrease, actionSave } from '../../redux/counterRedux'
 
 const CounterRedux = () => {
 
     const number = useSelector(state => state.counterReducer.number);
+    const save = useSelector(state => state.counterReducer.savestate);
     const dispatch = useDispatch();
 
     return (
         <div>
             <h4>Counter Redux</h4>
             <button onClick={e => dispatch(actionIncrease())}>Add</button>&nbsp;
-            <button onClick={e => dispatch(actionDecrease())}>Subtract</button>
-            <h3>Result: <span>{number}</span></h3>
+            <button onClick={e => dispatch(actionDecrease())}>Subtract</button>&nbsp;
+            <button onClick={e => dispatch(actionSave())}>Async Save</button>
+            <h3>Result: <span>{number}</span> {save !== '' ? <span>{save}</span> : <span></span>}</h3>
         </div>
     )
 }
--- src/redux/counterRedux.js
+++ src/redux/counterRedux.js
@@ -1,7 +1,10 @@
 // create action alias
 const ACTION_INCREASE = "ACTION_INCREASE";
 const ACTION_DECREASE = "ACTION_DECREASE";
-const ACTION_ASYNC_SAVE = "ACTION_ASYNC_SAVE";
+
+const ACTION_ASYNC_SAVE_STARTED = "ACTION_ASYNC_SAVE_STARTED";
+const ACTION_ASYNC_SAVE_SUCCEED = "ACTION_ASYNC_SAVE_SUCCEED";
+const ACTION_ASYNC_SAVE_FAIL = "ACTION_ASYNC_SAVE_FAIL";
 
 // create action and its payload
 export const actionIncrease = (num = 1) => ({
@@ -18,23 +21,69 @@ export const actionDecrease = (num = 1) => ({
     }
 })
 
-export const actionSave = () => ({
-    type: ACTION_ASYNC_SAVE
+export const actionAsyncSaveStarted = (message) => ({
+    type: ACTION_ASYNC_SAVE_STARTED,
+    payload: {
+        message: message
+    }
 })
 
+export const actionAsyncSaveSucceed = (message) => ({
+    type: ACTION_ASYNC_SAVE_SUCCEED,
+    payload: {
+        message: message
+    }    
+})
+
+export const actionAsyncSaveFail = (message) => ({
+    type: ACTION_ASYNC_SAVE_FAIL,
+    payload: {
+        error: message
+    }
+})
+
+export const actionSave = () => {
+    return (dispathObject) => {
+        dispathObject(actionAsyncSaveStarted("saving..."));
+        
+        const fakeRequest = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // resolve("save done")
+                reject("save failed");
+            }, 2000);
+        })
+
+        return fakeRequest
+            .then(result => {
+                console.log("success", result)
+                dispathObject(actionAsyncSaveSucceed(result))
+            })
+            .catch(error => {
+                console.log("error", error)
+                dispathObject(actionAsyncSaveFail(error))
+            }) 
+
+    }
+}
+
 const initialState = {
-    number: 0
+    number: 0,
+    savestate: ''
 }
 
 // create reducer for all the action
 export default (state = initialState, action) => {
     switch (action.type) {
         case ACTION_INCREASE:
-            return {...state, number: state.number + action.payload.num}            
+            return {...state, number: state.number + action.payload.num, savestate: "unsaved"} 
         case ACTION_DECREASE:
-            return {...state, number: state.number - action.payload.num}
-        case ACTION_ASYNC_SAVE:
-            break;
+            return {...state, number: state.number - action.payload.num, savestate: "unsaved"}
+        case ACTION_ASYNC_SAVE_STARTED:
+            return {...state, savestate: action.payload.message}
+        case ACTION_ASYNC_SAVE_SUCCEED:
+            return {...state, savestate: action.payload.message}
+        case ACTION_ASYNC_SAVE_FAIL:
+            return {...state, savestate: action.payload.error}
         default: return state;
     }
 }
