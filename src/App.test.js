22122ff1dea36e3ea764735b3ddad534ed3b1a23
BrianVo <vnbaotran@gmail.com>
2021-05-13 20:21:22 -0700

    tested counterRedux.js and test render properly

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { StrictMode } from 'react'
 import App from './App';
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>);
+  const eleArr = screen.getAllByText(/Counter Redux/i);
+  expect(eleArr).not.toBeNull();
+  expect(eleArr.length).toEqual(2);
 });
---
+++ src/redux/counterReduxToolkit.test.js
@@ -0,0 +1,75 @@
+import reducers, { actionIncrease, actionDecrease, actionAsyncSave } from './counterReduxToolkit';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+describe('testing state changes', () => {
+
+    const initialState = {number: 0, savestate: ''}
+
+    beforeAll(() => {})
+
+    afterAll(() => {})
+
+    it('increases 1 with no arguments', () => {
+        //Arrange
+        
+        const expectedState = {number: 1, savestate: 'unsaved'};
+
+        //Act
+        const state = reducers(initialState, actionIncrease());
+
+        //Assert
+        expect(state).toEqual(expectedState);
+    });
+
+    it('decreases 1 with no arguments', () => {
+        //Arrange
+        const expectedState = {number: -1, savestate: 'unsaved'};
+
+        //Act
+        const state = reducers(initialState, actionDecrease());
+
+        //Assert
+        expect(state).toEqual(expectedState);
+    });
+
+    it('increases according to argument', () => {
+        //Arrange
+        const expectedState = {number: 2, savestate: 'unsaved'};
+
+        //Act
+        const state = reducers(initialState, actionIncrease(2));
+
+        //Assert
+        expect(state).toEqual(expectedState);
+    })
+
+    it('decreases according to argument', () => {
+        //Arrange
+        const expectedState = {number: -2, savestate: 'unsaved'};
+
+        //Act
+        const state = reducers(initialState, actionDecrease(2));
+
+        //Assert
+        expect(state).toEqual(expectedState);
+    })
+})
+
+describe('testing asynchronous saving', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+
+    const initialState = {number: 12, savestate: 'unsaved'}
+    const store = mockStore(initialState)
+
+    it('called action save and failed', async () => {
+
+        //consequent actions
+        const expectedActions = [actionAsyncSaveStarted("saving..."), actionAsyncSaveFail('save failed')]
+
+        await store.dispatch(actionSave());
+
+        expect(store.getActions()).toEqual(expectedActions);
+    })
+})
